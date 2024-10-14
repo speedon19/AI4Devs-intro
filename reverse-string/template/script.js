270229c309e329bb692f1767964dbed2fdf53b39
¨LuisEUM¨ <luiseum95@gmail.com>
2024-10-14 21:37:51 +0200

    fixing template

--- reverse-string/template/script.js
+++
@@ -1,5 +0,0 @@
-function invertirTexto() {
-    var texto = document.getElementById('inputText').value;
-    var textoInvertido = texto.split('').reverse().join('');
-    document.getElementById('textoInvertido').textContent = textoInvertido;
-}
